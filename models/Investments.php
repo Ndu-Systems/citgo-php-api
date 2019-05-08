27649316e940c769530b2215544dc70bc7b7b243
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-05-08 14:49:15 +0200

    get active investment

---
+++ models/Investments.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Investments {
+      // DB Stuff
+      private $conn;
+
+        // properties
+        public $InvestmentId; 
+        public $Amount; 
+        public $Profit; 
+        public $Total; 
+        public $Name; 
+        public $Type; 
+        public $InvestmentDate; 
+        public $CreateUserId; 
+        public $ModifyUserId; 
+
+        // Consutructor
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+
+        // get active, closed, suspended investments for administrator
+        public function read($statusId)
+        {
+            $query = "SELECT * FROM investment where StatusId = ?";
+
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            // Execute query
+            $stmt->execute(array($statusId));
+            
+            return $stmt;
+        }
+}
