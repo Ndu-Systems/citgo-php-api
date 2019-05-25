c9a4b5e5b385e8a1f186642d01e93756f9b3ae94
freedomkhanyile <freedom.khanyile1@gmail.com>
2019-05-25 11:42:53 +0200

    db change

--- db/citgodb.sql
+++ db/citgodb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2019 at 03:47 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: May 24, 2019 at 10:52 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -49,7 +49,9 @@ CREATE TABLE `bankingdetails` (
 INSERT INTO `bankingdetails` (`BankingDetailsId`, `ClientId`, `BankName`, `BankBranch`, `AccountNumber`, `AccountType`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
 ('0b166cc0-729c-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', 'fnb', '2545', '878787878', 'c', '2019-05-09 22:50:13', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 22:50:13', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
 ('2596e3aa-7197-11e9-9e99-80fa5b45280e', 'User with email address (rewr@dsadsa) already exists', '32133', '3213', '213', '2332', '2019-05-08 15:42:39', 'User with email address (rewr@dsadsa) already exists', '2019-05-08 15:42:39', 'User with email address (rewr@dsadsa) already exists', 1),
+('3eb6bb63-7df8-11e9-b355-f48e38e878a3', '33c4a80a-7df8-11e9-b355-f48e38e878a3', 'FNB', '500', '12045335548745', 'Cheque', '2019-05-24 09:47:56', '33c4a80a-7df8-11e9-b355-f48e38e878a3', '2019-05-24 09:47:56', '33c4a80a-7df8-11e9-b355-f48e38e878a3', 1),
 ('4ff2c08b-7198-11e9-9e99-80fa5b45280e', '46af11e0-7198-11e9-9e99-80fa5b45280e', 'eqwrew', 'ewrwe', '332132', 'ewrewr', '2019-05-08 15:51:00', '46af11e0-7198-11e9-9e99-80fa5b45280e', '2019-05-08 15:51:00', '46af11e0-7198-11e9-9e99-80fa5b45280e', 1),
+('557582c6-740e-11e9-934a-f48e38e878a3', '493894fc-740e-11e9-934a-f48e38e878a3', 'FNB', '500', '120154032145', 'Cheque', '2019-05-11 19:00:23', '493894fc-740e-11e9-934a-f48e38e878a3', '2019-05-11 19:00:23', '493894fc-740e-11e9-934a-f48e38e878a3', 1),
 ('5d9406df-6f66-11e9-bde7-f48e38e878a3', '598a9063-6f66-11e9-bde7-f48e38e878a3', '32432', '423432', '4324', '42343', '2019-05-05 20:48:25', '598a9063-6f66-11e9-bde7-f48e38e878a3', '2019-05-05 20:48:25', '598a9063-6f66-11e9-bde7-f48e38e878a3', 1),
 ('7b4b409a-6f65-11e9-bde7-f48e38e878a3', '606563b9-6f65-11e9-bde7-f48e38e878a3', 'FAB', 'FAB', '23232', 'FAB', '2019-05-05 20:42:06', '606563b9-6f65-11e9-bde7-f48e38e878a3', '2019-05-05 20:42:06', '606563b9-6f65-11e9-bde7-f48e38e878a3', 1);
 
@@ -80,6 +82,8 @@ CREATE TABLE `beneficiaries` (
 INSERT INTO `beneficiaries` (`BeneficiaryId`, `ClientId`, `Name`, `Surname`, `IDNumber`, `Relation`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
 ('1727dc2d-729c-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', 'John', 'Doka', '47878787878', 'Father', '2019-05-09 22:50:34', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 22:50:34', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
 ('291b9f67-7197-11e9-9e99-80fa5b45280e', 'User with email address (rewr@dsadsa) already exists', '32133', '2133', '2133', '213', '2019-05-08 15:42:45', 'User with email address (rewr@dsadsa) already exists', '2019-05-08 15:42:45', 'User with email address (rewr@dsadsa) already exists', 1),
+('49728086-7df8-11e9-b355-f48e38e878a3', '33c4a80a-7df8-11e9-b355-f48e38e878a3', 'Sabelo', 'Ntombela', '92075540644674', 'Randburg', '2019-05-24 09:48:14', '33c4a80a-7df8-11e9-b355-f48e38e878a3', '2019-05-24 09:48:14', '33c4a80a-7df8-11e9-b355-f48e38e878a3', 1),
+('6c927d48-740e-11e9-934a-f48e38e878a3', '493894fc-740e-11e9-934a-f48e38e878a3', 'Freedom', 'KHanyile', '920513322457', 'Friend', '2019-05-11 19:01:01', '493894fc-740e-11e9-934a-f48e38e878a3', '2019-05-11 19:01:01', '493894fc-740e-11e9-934a-f48e38e878a3', 1),
 ('80858a06-6f65-11e9-bde7-f48e38e878a3', '606563b9-6f65-11e9-bde7-f48e38e878a3', 'dfdsf', 'fdsfs', '3213', 'sd', '2019-05-05 20:42:15', '606563b9-6f65-11e9-bde7-f48e38e878a3', '2019-05-05 20:42:15', '606563b9-6f65-11e9-bde7-f48e38e878a3', 1);
 
 -- --------------------------------------------------------
@@ -115,7 +119,10 @@ CREATE TABLE `clients` (
 
 INSERT INTO `clients` (`ClientId`, `UserId`, `FirstName`, `MiddleName`, `Surname`, `Province`, `Address`, `IDNumber`, `Gender`, `Country`, `City`, `PostCode`, `ParentId`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
 ('0255f480-729c-11e9-86f7-80fa5b45280e', '02559e87-729c-11e9-86f7-80fa5b45280e', 'Gerad', '', 'Boka', 'KZN', 'Not needed', '9654221114558', 'Male', 'South Africa', 'Durban', '0000', NULL, '2019-05-09 22:49:59', 'SYSTEM_WEB', '2019-05-09 22:49:59', 'SYSTEM_WEB', 1),
+('33c4a80a-7df8-11e9-b355-f48e38e878a3', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 'Nduduzo', 'Nduduzo', 'Khanyile', 'Gauteng', 'Not needed', '9.2072543350088E+14', 'Male', 'South Africa', 'Johannesburg', '0000', NULL, '2019-05-24 09:47:38', 'SYSTEM_WEB', '2019-05-24 09:47:38', 'SYSTEM_WEB', 4),
+('3a566eea-7e63-11e9-8458-f48e38e878a3', '3a5492aa-7e63-11e9-8458-f48e38e878a3', 'Nduduzo', 'dsfds', 'Magwaza', 'Gauteng', 'Not needed', '11', 'Female', 'Botswana', 'Johannesburg', '0000', NULL, '2019-05-24 22:33:45', 'SYSTEM_WEB', '2019-05-24 22:33:45', 'SYSTEM_WEB', 4),
 ('46af11e0-7198-11e9-9e99-80fa5b45280e', '46ae4982-7198-11e9-9e99-80fa5b45280e', 'sadad', 'sadsad', 'dssadsa', '32dsad', 'Not needed', '213213', 'Male', 'South Africa', 'fdsfds', '0000', NULL, '2019-05-08 15:50:44', 'SYSTEM_WEB', '2019-05-08 15:50:44', 'SYSTEM_WEB', 1),
+('493894fc-740e-11e9-934a-f48e38e878a3', '490b9b92-740e-11e9-934a-f48e38e878a3', 'Nduduzo', 'dsfds', 'Magwaza', 'Gauteng', 'Not needed', '9.0084503154243E+17', 'Male', 'South Africa', 'Johannesburg', '0000', NULL, '2019-05-11 19:00:02', 'SYSTEM_WEB', '2019-05-11 19:00:02', 'SYSTEM_WEB', 1),
 ('598a9063-6f66-11e9-bde7-f48e38e878a3', '5989f18f-6f66-11e9-bde7-f48e38e878a3', 'Nduduzo', 'dsfds', 'Magwaza', 'Gauteng', 'Angular STREETS', '3432432', 'Male', 'South Africa', 'Johannesburg', '2194', NULL, '2019-05-05 20:48:19', 'SYSTEM_WEB', '2019-05-05 20:48:19', 'SYSTEM_WEB', 1),
 ('606563b9-6f65-11e9-bde7-f48e38e878a3', '60622a96-6f65-11e9-bde7-f48e38e878a3', 'Nduduzo', NULL, 'Magwaza', 'Gauteng', 'Angular STREETS', '3242343243', 'Male', 'South Africa', 'Johannesburg', '2194', NULL, '2019-05-05 20:41:21', 'SYSTEM_WEB', '2019-05-05 20:41:21', 'SYSTEM_WEB', 1),
 ('8d82242c-61af-11e9-ac92-80fa5b45280e', '8d81d1a0-61af-11e9-ac92-80fa5b45280e', 'lkjll', 'jkll', 'jkl', 'hgfh', 'hgfh', '78658', 'Female', 'South Africa', 'g', '566', NULL, '2019-04-18 09:57:03', 'SYSTEM_WEB', '2019-04-18 09:57:03', 'SYSTEM_WEB', 1),
@@ -186,6 +193,7 @@ INSERT INTO `investment` (`InvestmentId`, `ClientId`, `Amount`, `Profit`, `Total
 ('1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '25000', '0', '0', 'New makes', 'Month-to-month', '2019-05-08 22:51:29', '2019-05-08 22:51:29', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-08 22:51:29', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 2),
 ('40a86f1a-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1200', '0', '0', 'New Investor', 'Yearly', '2019-05-08 22:52:27', '2019-05-08 22:52:27', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-08 22:52:27', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 2),
 ('5030a897-729f-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '6000', '0', '0', 'test 2', 'Month-to-month', '2019-05-09 23:13:38', '2019-05-09 23:13:38', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:13:38', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
+('51253468-740f-11e9-934a-f48e38e878a3', '493894fc-740e-11e9-934a-f48e38e878a3', '5500', '0', '0', 'BEE Empower', 'Month-to-month', '2019-05-11 19:07:25', '2019-05-11 19:07:25', '493894fc-740e-11e9-934a-f48e38e878a3', '2019-05-11 19:07:25', '493894fc-740e-11e9-934a-f48e38e878a3', 2),
 ('5dd0671c-729e-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '5000', '0', '0', 'Ndu car', 'Month-to-month', '2019-05-09 23:06:51', '2019-05-09 23:06:51', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:06:51', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
 ('7dd60329-729f-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '5600', '0', '0', 'test 4', 'Month-to-month', '2019-05-09 23:14:54', '2019-05-09 23:14:54', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:14:54', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
 ('8f28a33b-72a1-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '88888', '0', '0', 'test8888', 'Month-to-month', '2019-05-09 23:29:42', '2019-05-09 23:29:42', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:29:42', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
@@ -302,7 +310,7 @@ CREATE TABLE `userroles` (
   `RoleId` varchar(225) NOT NULL,
   `CreateDate` datetime NOT NULL,
   `CreateUserId` varchar(225) NOT NULL,
-  `ModifyDate` datetime NOT NULL,
+  `ModifyDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ModifyUserId` varchar(225) NOT NULL,
   `StatusId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -313,7 +321,17 @@ CREATE TABLE `userroles` (
 
 INSERT INTO `userroles` (`Id`, `UserId`, `RoleId`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
 (1, '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2', '2019-05-06 00:00:00', 'me', '2019-05-06 00:00:00', 'me', 1),
-(2, '02559e87-729c-11e9-86f7-80fa5b45280e', '2', '2019-05-06 00:00:00', 'me', '2019-05-06 00:00:00', 'me', 1);
+(2, '02559e87-729c-11e9-86f7-80fa5b45280e', '2', '2019-05-06 00:00:00', 'me', '2019-05-06 00:00:00', 'me', 1),
+(3, '490b9b92-740e-11e9-934a-f48e38e878a3', '2', '2019-05-06 00:00:00', 'me', '2019-05-06 00:00:00', 'me', 1),
+(4, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(5, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(6, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(7, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(8, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(9, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(10, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(11, '33c1cad0-7df8-11e9-b355-f48e38e878a3', '2', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', '0000-00-00 00:00:00', '33c1cad0-7df8-11e9-b355-f48e38e878a3', 1),
+(12, '3a5492aa-7e63-11e9-8458-f48e38e878a3', '2', '0000-00-00 00:00:00', '3a5492aa-7e63-11e9-8458-f48e38e878a3', '0000-00-00 00:00:00', '3a5492aa-7e63-11e9-8458-f48e38e878a3', 1);
 
 -- --------------------------------------------------------
 
@@ -341,7 +359,10 @@ INSERT INTO `users` (`UserId`, `Email`, `CellphoneNumber`, `Password`, `CreateDa
 ('02559e87-729c-11e9-86f7-80fa5b45280e', 'gerad@mail.co.za', '025488455888', 'pass@123!', '2019-05-09 22:49:59', 'SYSTEM_WEB', '2019-05-09 22:49:59', 'SYSTEM_WEB', 1),
 ('19c58f7b-6f68-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass@123!', '2019-05-05 21:00:51', 'SYSTEM_WEB', '2019-05-05 21:00:51', 'SYSTEM_WEB', 1),
 ('2083b0d9-7197-11e9-9e99-80fa5b45280e', 'rewr@dsadsa', '321321', 'pass@123!', '2019-05-08 15:42:31', 'SYSTEM_WEB', '2019-05-08 15:42:31', 'SYSTEM_WEB', 1),
+('33c1cad0-7df8-11e9-b355-f48e38e878a3', 'freedom.khanyile1@gmail.com', '0748508258', 'pass@123!', '2019-05-24 09:47:38', 'SYSTEM_WEB', '2019-05-24 09:47:38', 'SYSTEM_WEB', 3),
+('3a5492aa-7e63-11e9-8458-f48e38e878a3', 'magwaza@mail.com11', '0748508258', 'pass@123!', '2019-05-24 22:33:45', 'SYSTEM_WEB', '2019-05-24 22:33:45', 'SYSTEM_WEB', 3),
 ('46ae4982-7198-11e9-9e99-80fa5b45280e', 'ddadasd@dsdsad', '3213', 'pass@123!', '2019-05-08 15:50:44', 'SYSTEM_WEB', '2019-05-08 15:50:44', 'SYSTEM_WEB', 1),
+('490b9b92-740e-11e9-934a-f48e38e878a3', 'freedom.khanyile@ndu-system.net', '0748508258', 'pass@123!', '2019-05-11 19:00:02', 'SYSTEM_WEB', '2019-05-11 19:00:02', 'SYSTEM_WEB', 1),
 ('5989f18f-6f66-11e9-bde7-f48e38e878a3', 'freedom@mail.com', '0748508258', 'pass@123!', '2019-05-05 20:48:19', 'SYSTEM_WEB', '2019-05-05 20:48:19', 'SYSTEM_WEB', 1),
 ('60622a96-6f65-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass@123!', '2019-05-05 20:41:21', 'SYSTEM_WEB', '2019-05-05 20:41:21', 'SYSTEM_WEB', 1),
 ('89cb1440-7197-11e9-9e99-80fa5b45280e', 'sad@dsdsd3', '213', 'pass@123!', '2019-05-08 15:45:27', 'SYSTEM_WEB', '2019-05-08 15:45:27', 'SYSTEM_WEB', 1),
@@ -470,6 +491,12 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `statuses`
   MODIFY `StatusId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userroles`
+--
+ALTER TABLE `userroles`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
