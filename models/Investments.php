ecdf97539d4e079f08d38423202a7a2df4769189
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-27 10:45:36 +0200

    investemnt docs

--- models/Investments.php
+++ models/Investments.php
@@ -1,64 +1,66 @@
-<?php 
+<?php
 
-class Investments {
-      // DB Stuff
-      private $conn;
+class Investments
+{
+    // DB Stuff
+    private $conn;
 
-        // Consutructor
-        public function __construct($db) {
-            $this->conn = $db;
-        }
+    // Consutructor
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
 
-        // get active, closed, suspended investments for administrator
-        public function read($statusId)
-        {
-            $query = "SELECT * FROM investment where StatusId = ?";
+    // get active, closed, suspended investments for administrator
+    public function read($statusId)
+    {
+        $query = "SELECT * FROM investment where StatusId = ?";
 
-            // prepare query statement
-            $stmt = $this->conn->prepare($query);
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
 
-            // Execute query
-            $stmt->execute(array($statusId));
-            
-            return $stmt;
-        }
+        // Execute query
+        $stmt->execute(array($statusId));
 
-        // insert investment
-        public function insert(
-            $Amount, 
-            $ClientId,        
-            $Name, 
-            $Type        
-        )
-        {
-           $query = "INSERT INTO investment(InvestmentId, ClientId,Amount, Profit, Total, Name, Type, InvestmentDate, CreateUserId, ModifyUserId, StatusId) 
+        return $stmt;
+    }
+
+    // insert investment
+    public function insert(
+        $Amount,
+        $ClientId,
+        $Name,
+        $Type
+    ) {
+        $query = "INSERT INTO investment(InvestmentId, ClientId,Amount, Profit, Total, Name, Type, InvestmentDate, CreateUserId, ModifyUserId, StatusId) 
                     VALUES (uuid(),?,?,0,0 ,? ,?,Now() ,? ,? ,? )";
-            $stmt= $this->conn->prepare($query);
-           
-            try {
-                //code...
-                if(
-                    $stmt->execute(array(
-                        $ClientId,
-                        $Amount, 
-                        $Name, 
-                        $Type, 
-                        $ClientId,  
-                        $ClientId,  
-                        2
-        
-                    ))){
-                        return 1;
-                    }
-            } catch (Exception $ex) {
-                return $ex;
-            }
-        }
+        $stmt = $this->conn->prepare($query);
 
-        public function readByClientId($ClientId)
-        {
-            $query = "
+        try {
+            //code...
+            if (
+                $stmt->execute(array(
+                    $ClientId,
+                    $Amount,
+                    $Name,
+                    $Type,
+                    $ClientId,
+                    $ClientId,
+                    2
+
+                ))
+            ) {
+                return 1;
+            }
+        } catch (Exception $ex) {
+            return $ex;
+        }
+    }
+
+    public function readByClientId($ClientId)
+    {
+        $query = "
             SELECT 
             InvestmentId,
             ClientId,
@@ -76,34 +78,36 @@ class Investments {
             CASE
             WHEN StatusId = 1 THEN 'ACTIVE'
             WHEN StatusId = 2 THEN 'PENDING'
+            WHEN StatusId = 3 THEN 'AWAITING-CONFIRMATION'
             ELSE 'INACTIVE'
             END AS Status
             FROM
             investment
-            where ClientId = ? AND StatusId in (1,2)";
+            where ClientId = ? AND StatusId in (1,2,3)
+            order by  CreateDate desc
+            ";
 
-            // prepare query statement
-            $stmt = $this->conn->prepare($query);
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
 
-            // Execute query
-            $stmt->execute(array($ClientId));
-            
-            return $stmt;
-        }
+        // Execute query
+        $stmt->execute(array($ClientId));
 
-        public function updateInvestment(
-            $ClientId,
-            $Amount,
-            $Profit,
-            $Total,
-            $Name,
-            $Type,
-            $ModifyUserId,
-            $StatusId,
-            $InvestmentId
-        )
-        {
-            $query = "
+        return $stmt;
+    }
+
+    public function updateInvestment(
+        $ClientId,
+        $Amount,
+        $Profit,
+        $Total,
+        $Name,
+        $Type,
+        $ModifyUserId,
+        $StatusId,
+        $InvestmentId
+    ) {
+        $query = "
             UPDATE investment SET  
                 ClientId=?, 
                 Amount=?, 
@@ -118,28 +122,28 @@ class Investments {
                 InvestmentId =?
 
             ";
-           
-            try {
-                $stmt = $this->conn->prepare($query);          
-                if ($stmt->execute(array(
-                    $ClientId,
-                    $Amount,
-                    $Profit,
-                    $Total,
-                    $Name,
-                    $Type,
-                    $ModifyUserId,
-                    $StatusId,
-                    $InvestmentId
-                ))) {
-                    return $this->getInvestmentById($InvestmentId);
-                }
-            } catch (Exception $e) {
-                return $e;
+
+        try {
+            $stmt = $this->conn->prepare($query);
+            if ($stmt->execute(array(
+                $ClientId,
+                $Amount,
+                $Profit,
+                $Total,
+                $Name,
+                $Type,
+                $ModifyUserId,
+                $StatusId,
+                $InvestmentId
+            ))) {
+                return $this->getInvestmentById($InvestmentId);
             }
+        } catch (Exception $e) {
+            return $e;
         }
-    
-        
+    }
+
+
     public function getInvestmentById($InvestmentId)
     {
         $query = "SELECT * FROM investment WHERE InvestmentId =?";
@@ -149,6 +153,28 @@ class Investments {
 
         if ($stmt->rowCount()) {
             $emails = $stmt->fetch(PDO::FETCH_ASSOC);
+            return  $emails;
+        }
+    }
+    public function getInvestmentAndDocumentsById($InvestmentId)
+    {
+        $query = "SELECT *,
+        CASE
+            WHEN i.StatusId = 1 THEN 'ACTIVE'
+            WHEN  i.StatusId = 2 THEN 'PENDING'
+            WHEN  i.StatusId = 3 THEN 'AWAITING-CONFIRMATION'
+            ELSE 'INACTIVE'
+            END AS Status,
+            i.CreateDate as InvestmentDate
+         FROM investment i left join documents d 
+                   on i.InvestmentId = d.InvestmentId 
+                  WHERE i.InvestmentId =?";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute(array($InvestmentId));
+
+        if ($stmt->rowCount()) {
+            $emails = $stmt->fetchAll(PDO::FETCH_ASSOC);
             return  $emails;
         }
     }
