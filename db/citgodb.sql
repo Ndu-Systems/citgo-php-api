c25156009f78769d3e24e3093b7d47870f2aaf15
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-05-27 13:52:18 +0200

    admin role

--- db/citgodb.sql
+++ db/citgodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2019 at 12:39 AM
+-- Generation Time: May 27, 2019 at 01:51 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -137,7 +137,7 @@ INSERT INTO `clients` (`ClientId`, `UserId`, `FirstName`, `MiddleName`, `Surname
 ('61e541de-7f0f-11e9-a700-80fa5b45280e', '61e4ec36-7f0f-11e9-a700-80fa5b45280e', 'John22', NULL, 'Doe22', 'hgfh', 'Not needed', '22', 'Female', 'South Africa', 'Johanesburg', '0000', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-25 19:06:06', 'SYSTEM_WEB', '2019-05-25 19:06:06', 'SYSTEM_WEB', 4),
 ('8855e70e-7ee9-11e9-a700-80fa5b45280e', '8853cc2c-7ee9-11e9-a700-80fa5b45280e', 'John', NULL, 'Doe', 'hgfh', 'Not needed', '4545', 'Male', 'South Africa', 'Johanesburg', '0000', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-25 14:35:10', 'SYSTEM_WEB', '2019-05-25 14:35:10', 'SYSTEM_WEB', 4),
 ('8d82242c-61af-11e9-ac92-80fa5b45280e', '8d81d1a0-61af-11e9-ac92-80fa5b45280e', 'lkjll', 'jkll', 'jkl', 'hgfh', 'hgfh', '78658', 'Female', 'South Africa', 'g', '566', NULL, '2019-04-18 09:57:03', 'SYSTEM_WEB', '2019-04-18 09:57:03', 'SYSTEM_WEB', 1),
-('a39d846a-61b3-11e9-ac92-80fa5b452iie', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 'Nkuleko', 'Test', 'Testing', 'Gauteng', '', '925042254335466648', 'Male', 'South Africa', 'fdfd', '434', NULL, '2019-04-18 10:26:19', 'SYSTEM_WEB', '2019-04-18 10:26:19', 'SYSTEM_WEB', 1),
+('a39d846a-61b3-11e9-ac92-80fa5b452iie', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 'John', 'Test', 'Doe', 'Gauteng', 'Test address stret 123', '925042254335466648', 'Male', 'South Africa', 'fdfd', '434', NULL, '2019-04-18 10:26:19', 'SYSTEM_WEB', '2019-04-18 10:26:19', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
 ('af22bccf-7f16-11e9-a700-80fa5b45280e', 'af224651-7f16-11e9-a700-80fa5b45280e', 'John', NULL, 'Doe', 'hgfh', 'Not needed', '213132', 'Male', 'South Africa', 'Johanesburg', '0000', '', '2019-05-25 19:58:22', 'SYSTEM_WEB', '2019-05-25 19:58:22', 'SYSTEM_WEB', 4),
 ('c40d6651-7ee7-11e9-a700-80fa5b45280e', 'c40d0e11-7ee7-11e9-a700-80fa5b45280e', 'John', NULL, 'Ref', 'hgfh', 'Not needed', '213213', 'Female', 'South Africa', 'Johanesburg', '0000', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-25 14:22:31', 'SYSTEM_WEB', '2019-05-25 14:22:31', 'SYSTEM_WEB', 4);
 
@@ -183,8 +183,18 @@ CREATE TABLE `documents` (
 --
 
 INSERT INTO `documents` (`DocumentId`, `DocumentCode`, `ClientId`, `InvestmentId`, `DocumentName`, `DocumentUrl`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
+('142c9a37-804a-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 'f4b41d7f-8048-11e9-b26a-80fa5b45280e', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558939125smartmockups_jvw1k73g.jpg', '2019-05-27 08:38:45', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:38:45', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
 ('17466d21-7f3d-11e9-a359-80fa5b45280e', 'a39d846a-6', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-26 00:33:17', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-26 00:33:17', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 0),
-('54847806-7f3d-11e9-a359-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/15588236994.jpg', '2019-05-26 00:34:59', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-26 00:34:59', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1);
+('1a24fdfe-804a-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 'f4b41d7f-8048-11e9-b26a-80fa5b45280e', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558939135smartmockups_jvw1k73g.jpg', '2019-05-27 08:38:55', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:38:55', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('2850c711-8047-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558937871cigo.PNG', '2019-05-27 08:17:51', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:17:51', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('3b757566-8048-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938332random2.PNG', '2019-05-27 08:25:32', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:25:32', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('54847806-7f3d-11e9-a359-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/15588236994.jpg', '2019-05-26 00:34:59', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-26 00:34:59', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('8efe1e7e-8047-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938043cigo2.PNG', '2019-05-27 08:20:43', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:20:43', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('92c5a985-8047-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938049cigo2.PNG', '2019-05-27 08:20:49', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:20:49', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('aaf55e7b-804b-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '10099979-804b-11e9-b26a-80fa5b45280e', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558939808smartmockups_jvw1k73g.jpg', '2019-05-27 08:50:08', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:50:08', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('c14f7d40-8047-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938127cigo.PNG', '2019-05-27 08:22:07', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:22:07', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('c25a2189-8048-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '40a86f1a-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938559smartmockups_jvw1k73g.jpg', '2019-05-27 08:29:19', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:29:19', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
+('f6268248-8047-11e9-b26a-80fa5b45280e', 'POP', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'Proof of payment', 'http://localhost:8080/citgo-php-api/api/upload/uploads/1558938216smartmockups_jvw1k73g - Copy.jpg', '2019-05-27 08:23:36', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', '2019-05-27 08:23:36', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1);
 
 -- --------------------------------------------------------
 
@@ -213,9 +223,10 @@ CREATE TABLE `investment` (
 --
 
 INSERT INTO `investment` (`InvestmentId`, `ClientId`, `Amount`, `Profit`, `Total`, `Name`, `Type`, `InvestmentDate`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
+('10099979-804b-11e9-b26a-80fa5b45280e', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '32', '0', '0', 'Share 6', 'Month-to-month', '2019-05-27 08:45:48', '2019-05-27 08:45:48', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-27 08:50:08', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 3),
 ('12213b75-7f1c-11e9-a700-80fa5b45280e', 'af22bccf-7f16-11e9-a700-80fa5b45280e', '6700', '0', '0', 'Share 1', 'Month-to-month', '2019-05-25 20:36:56', '2019-05-25 20:36:56', 'af22bccf-7f16-11e9-a700-80fa5b45280e', '2019-05-25 20:36:56', 'af22bccf-7f16-11e9-a700-80fa5b45280e', 2),
-('1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '25000', '0', '0', 'New makes', 'Month-to-month', '2019-05-08 22:51:29', '2019-05-08 22:51:29', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-08 22:51:29', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 2),
-('40a86f1a-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1200', '0', '0', 'New Investor', 'Yearly', '2019-05-08 22:52:27', '2019-05-08 22:52:27', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-08 22:52:27', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 2),
+('1e5d8a52-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '25000', '0', '0', 'New makes', 'Month-to-month', '2019-05-08 22:51:29', '2019-05-08 22:51:29', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-27 08:25:32', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 3),
+('40a86f1a-71d3-11e9-ba03-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '1200', '0', '0', 'New Investor', 'Yearly', '2019-05-08 22:52:27', '2019-05-08 22:52:27', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-27 08:29:19', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 3),
 ('5030a897-729f-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '6000', '0', '0', 'test 2', 'Month-to-month', '2019-05-09 23:13:38', '2019-05-09 23:13:38', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:13:38', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
 ('51253468-740f-11e9-934a-f48e38e878a3', '493894fc-740e-11e9-934a-f48e38e878a3', '5500', '0', '0', 'BEE Empower', 'Month-to-month', '2019-05-11 19:07:25', '2019-05-11 19:07:25', '493894fc-740e-11e9-934a-f48e38e878a3', '2019-05-11 19:07:25', '493894fc-740e-11e9-934a-f48e38e878a3', 2),
 ('5dd0671c-729e-11e9-86f7-80fa5b45280e', '0255f480-729c-11e9-86f7-80fa5b45280e', '5000', '0', '0', 'Ndu car', 'Month-to-month', '2019-05-09 23:06:51', '2019-05-09 23:06:51', '0255f480-729c-11e9-86f7-80fa5b45280e', '2019-05-09 23:06:51', '0255f480-729c-11e9-86f7-80fa5b45280e', 1),
@@ -231,7 +242,8 @@ INSERT INTO `investment` (`InvestmentId`, `ClientId`, `Amount`, `Profit`, `Total
 ('cfb1bc3f-7293-11e9-8d37-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '8000', '0', '0', 'BUY HOUSE', 'Yearly', '2019-05-09 21:51:18', '2019-05-09 21:51:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-09 21:51:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 1),
 ('d2507a51-701c-11e9-9519-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b45280e', '5000', '0', '0', 'Test Investment 2', 'Yearly', '2019-05-06 18:34:30', '2019-05-06 18:34:30', '1', '2019-05-06 18:34:30', '1', 1),
 ('d2507a51-701c-11e9-9519-f48e58e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b45280e', '600', '0', '0', 'Test Investment 2', 'Yearly', '2019-05-06 18:34:30', '2019-05-06 18:34:30', '1', '2019-05-06 18:34:30', '1', 1),
-('f3c5ac5c-7293-11e9-8d37-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '5000', '0', '0', 'NEW SOCCER KIT', 'Yearly', '2019-05-09 21:52:18', '2019-05-09 21:52:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-09 21:52:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 1);
+('f3c5ac5c-7293-11e9-8d37-f48e38e878a3', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '5000', '0', '0', 'NEW SOCCER KIT', 'Yearly', '2019-05-09 21:52:18', '2019-05-09 21:52:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-09 21:52:18', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 1),
+('f4b41d7f-8048-11e9-b26a-80fa5b45280e', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '3232', '0', '0', 'Share 6', 'Month-to-month', '2019-05-27 08:30:43', '2019-05-27 08:30:43', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', '2019-05-27 08:38:56', 'a39d846a-61b3-11e9-ac92-80fa5b452iie', 3);
 
 -- --------------------------------------------------------
 
@@ -287,6 +299,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`RoleId`, `Description`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
+(1, 'Admin', '2019-05-06 21:48:02', 'me', '2019-05-06 21:48:02', 'me', 1),
 (2, 'Client', '2019-05-06 21:48:02', 'me', '2019-05-06 21:48:02', 'me', 1);
 
 -- --------------------------------------------------------
@@ -360,7 +373,8 @@ INSERT INTO `userroles` (`Id`, `UserId`, `RoleId`, `CreateDate`, `CreateUserId`,
 (13, '8853cc2c-7ee9-11e9-a700-80fa5b45280e', '2', '0000-00-00 00:00:00', '8853cc2c-7ee9-11e9-a700-80fa5b45280e', '2019-05-25 14:36:04', '8853cc2c-7ee9-11e9-a700-80fa5b45280e', 1),
 (14, '3521db6c-7f04-11e9-a700-80fa5b45280e', '2', '0000-00-00 00:00:00', '3521db6c-7f04-11e9-a700-80fa5b45280e', '2019-05-25 17:55:27', '3521db6c-7f04-11e9-a700-80fa5b45280e', 1),
 (15, '61e4ec36-7f0f-11e9-a700-80fa5b45280e', '2', '0000-00-00 00:00:00', '61e4ec36-7f0f-11e9-a700-80fa5b45280e', '2019-05-25 19:07:15', '61e4ec36-7f0f-11e9-a700-80fa5b45280e', 1),
-(16, 'af224651-7f16-11e9-a700-80fa5b45280e', '2', '0000-00-00 00:00:00', 'af224651-7f16-11e9-a700-80fa5b45280e', '2019-05-25 20:08:03', 'af224651-7f16-11e9-a700-80fa5b45280e', 1);
+(16, 'af224651-7f16-11e9-a700-80fa5b45280e', '2', '0000-00-00 00:00:00', 'af224651-7f16-11e9-a700-80fa5b45280e', '2019-05-25 20:08:03', 'af224651-7f16-11e9-a700-80fa5b45280e', 1),
+(17, '60622a96-6f65-11e9-bde7-f48e38e878a3', '1', '0000-00-00 00:00:00', 'af224651-7f16-11e9-a700-80fa5b45280e', '2019-05-25 20:08:03', 'af224651-7f16-11e9-a700-80fa5b45280e', 1);
 
 -- --------------------------------------------------------
 
@@ -386,7 +400,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`UserId`, `Email`, `CellphoneNumber`, `Password`, `CreateDate`, `CreateUserId`, `ModifyDate`, `ModifyUserId`, `StatusId`) VALUES
 ('02559e87-729c-11e9-86f7-80fa5b45280e', 'gerad@mail.co.za', '025488455888', 'pass@123!', '2019-05-09 22:49:59', 'SYSTEM_WEB', '2019-05-09 22:49:59', 'SYSTEM_WEB', 1),
-('19c58f7b-6f68-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass@123!s', '2019-05-05 21:00:51', 'SYSTEM_WEB', '2019-05-05 21:00:51', 'SYSTEM_WEB', 1),
+('19c58f7b-6f68-11e9-bde7-f48e38e878a3', 'john.doe@mail.com', '0725698888', 'pass', '2019-05-05 21:00:51', 'SYSTEM_WEB', '2019-05-05 21:00:51', '19c58f7b-6f68-11e9-bde7-f48e38e878a3', 1),
 ('2083b0d9-7197-11e9-9e99-80fa5b45280e', 'rewr@dsadsa', '321321', 'pass@123!', '2019-05-08 15:42:31', 'SYSTEM_WEB', '2019-05-08 15:42:31', 'SYSTEM_WEB', 1),
 ('2e4f37c6-7ee8-11e9-a700-80fa5b45280e', 'john.doe@mail.com3', '0725698888', 'pass@123!', '2019-05-25 14:25:29', 'SYSTEM_WEB', '2019-05-25 14:25:29', 'SYSTEM_WEB', 4),
 ('33c1cad0-7df8-11e9-b355-f48e38e878a3', 'freedom.khanyile1@gmail.com', '0748508258', 'pass@123!', '2019-05-24 09:47:38', 'SYSTEM_WEB', '2019-05-24 09:47:38', 'SYSTEM_WEB', 3),
@@ -396,7 +410,7 @@ INSERT INTO `users` (`UserId`, `Email`, `CellphoneNumber`, `Password`, `CreateDa
 ('473f9c14-7ee8-11e9-a700-80fa5b45280e', 'john.doe@mail.com332', '0725698888', 'pass@123!', '2019-05-25 14:26:11', 'SYSTEM_WEB', '2019-05-25 14:26:11', 'SYSTEM_WEB', 4),
 ('490b9b92-740e-11e9-934a-f48e38e878a3', 'freedom.khanyile@ndu-system.net', '0748508258', 'pass@123!', '2019-05-11 19:00:02', 'SYSTEM_WEB', '2019-05-11 19:00:02', 'SYSTEM_WEB', 1),
 ('5989f18f-6f66-11e9-bde7-f48e38e878a3', 'freedom@mail.com', '0748508258', 'pass@123!', '2019-05-05 20:48:19', 'SYSTEM_WEB', '2019-05-05 20:48:19', 'SYSTEM_WEB', 1),
-('60622a96-6f65-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass@123!', '2019-05-05 20:41:21', 'SYSTEM_WEB', '2019-05-05 20:41:21', 'SYSTEM_WEB', 1),
+('60622a96-6f65-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass', '2019-05-05 20:41:21', 'SYSTEM_WEB', '2019-05-05 20:41:21', 'SYSTEM_WEB', 1),
 ('61e4ec36-7f0f-11e9-a700-80fa5b45280e', 'john.doe@mail.com2', '0725698888', 'pass', '2019-05-25 19:06:06', 'SYSTEM_WEB', '2019-05-25 19:06:06', 'SYSTEM_WEB', 3),
 ('8853cc2c-7ee9-11e9-a700-80fa5b45280e', 'john.doe@mail.coms', '0725698888', 'pass', '2019-05-25 14:35:10', 'SYSTEM_WEB', '2019-05-25 14:35:10', 'SYSTEM_WEB', 3),
 ('89cb1440-7197-11e9-9e99-80fa5b45280e', 'sad@dsdsd3', '213', 'pass@123!', '2019-05-08 15:45:27', 'SYSTEM_WEB', '2019-05-08 15:45:27', 'SYSTEM_WEB', 1),
@@ -406,8 +420,7 @@ INSERT INTO `users` (`UserId`, `Email`, `CellphoneNumber`, `Password`, `CreateDa
 ('af224651-7f16-11e9-a700-80fa5b45280e', 'john.doe@mail.com33', '0725698888', 'pass', '2019-05-25 19:58:22', 'SYSTEM_WEB', '2019-05-25 19:58:22', 'SYSTEM_WEB', 3),
 ('b0e825d1-7197-11e9-9e99-80fa5b45280e', 'wee@dsdsad2332321.com', '321321', 'pass@123!', '2019-05-08 15:46:33', 'SYSTEM_WEB', '2019-05-08 15:46:33', 'SYSTEM_WEB', 1),
 ('b37eb2ca-61b0-11e9-ac92-80fa5b45280e', 'fsd@DSAD', '343', 'pass@123!', '2019-04-18 10:05:17', 'SYSTEM_WEB', '2019-04-18 10:05:17', 'SYSTEM_WEB', 1),
-('c40d0e11-7ee7-11e9-a700-80fa5b45280e', 'john.doe@mail.com', '0725698888', 'pass@123!', '2019-05-25 14:22:31', 'SYSTEM_WEB', '2019-05-25 14:22:31', 'SYSTEM_WEB', 4),
-('f1dc8dbf-6f67-11e9-bde7-f48e38e878a3', 'magwaza@mail.com', '0748508258', 'pass@123!', '2019-05-05 20:59:44', 'SYSTEM_WEB', '2019-05-05 20:59:44', 'SYSTEM_WEB', 1);
+('c40d0e11-7ee7-11e9-a700-80fa5b45280e', 'john.doe@mail.com', '0725698888', 'pass@123!', '2019-05-25 14:22:31', 'SYSTEM_WEB', '2019-05-25 14:22:31', 'SYSTEM_WEB', 4);
 
 -- --------------------------------------------------------
 
@@ -532,7 +545,7 @@ ALTER TABLE `statuses`
 -- AUTO_INCREMENT for table `userroles`
 --
 ALTER TABLE `userroles`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
