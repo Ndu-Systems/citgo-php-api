f2db1f7ee5d55df51577c561a3a2f73fa1dcb674
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-06-27 23:51:58 +0200

    get growth on the dektop

--- api/withdrawal/add-withdrawal.php
+++ api/withdrawal/add-withdrawal.php
@@ -5,9 +5,9 @@ include_once '../../models/Withdrawal.php';
 $data = json_decode(file_get_contents("php://input"));
 
 $Amount=$data->Amount;
-$CreateUserId=$data->Amount;
-$ModifyUserId=$data->Amount;
-$StatusId=$data->Amount;
+$CreateUserId=$data->CreateUserId;
+$ModifyUserId=$data->ModifyUserId;
+$StatusId=$data->StatusId;
 //connect to db
 $database = new Database();
 $db = $database->connect();
